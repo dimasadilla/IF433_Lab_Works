291c3af47d0beac184152c7b74f465e2a1ee9038
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-06 10:23:16 +0700

    week11: implement apply fot object initialization

--- src/oop_75794_Week11_LeonardusAdilla/ScopeConfiguration.kt
+++ src/oop_75794_Week11_LeonardusAdilla/ScopeConfiguration.kt
@@ -1,3 +1,12 @@
 package oop_75794_Week11_LeonardusAdilla
 
 data class User(var name: String = "", var age : Int= 0)
+
+fun main () {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println (user)
+}
