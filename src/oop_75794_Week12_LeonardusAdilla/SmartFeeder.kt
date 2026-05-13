99952bc8b4418b7488255e9204682c8b24721fc0
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-13 10:28:30 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_75794_Week12_LeonardusAdilla/SmartFeeder.kt
+++ src/oop_75794_Week12_LeonardusAdilla/SmartFeeder.kt
@@ -67,5 +67,13 @@ fun main() {
             isJammed = false
         )
 
+    }.onSuccess { newStock ->
+
+        currentKibbleStock = newStock
+
+        println(
+            "Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr"
+        )
+
     }
 }
