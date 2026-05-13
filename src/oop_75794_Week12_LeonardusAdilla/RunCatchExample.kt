586f05137f13b0b4c62e8a777007724c21636562
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-13 09:22:39 +0700

    week12: implement onSuccess and onFailure  chaining

--- src/oop_75794_Week12_LeonardusAdilla/RunCatchExample.kt
+++ src/oop_75794_Week12_LeonardusAdilla/RunCatchExample.kt
@@ -1,16 +1,27 @@
 package oop_75794_Week12_LeonardusAdilla
 
 fun main() {
+
     println("=== TEST RUNCATCHING ===")
+
     val result: Result<Int> = runCatching {
         "42X".toInt()
     }
 
-    //Pattern: getOrElse
-    val safeValue = result.getOrElse {-1}
-    println("safe Value (getOrElse): $safeValue")
+    // Pattern: getOrElse
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
 
-    // pattern: recover (bisa mengubah tipe kegagalan jadi sukses)
-    val recovered = result.recover {0}.getOrNull()
+    // Pattern: recover
+    val recovered = result.recover { 0 }.getOrNull()
     println("Recovered Value: $recovered")
+
+    // runCatching kedua
+    runCatching {
+        "100".toInt()
+    }.onSuccess { v ->
+        println("Berhasil dikonversi: $v")
+    }.onFailure { e ->
+        println("Gagal konversi: ${e.message}")
+    }
 }
