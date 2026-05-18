ca0d2ac5e75f375b7ff7e173ccb54faa051355ae
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-18 21:27:56 +0700

    week13: test complete student serialization pipeline

--- src/oop_75794_Week13_LeonardusAdilla/StudentSerialization.kt
+++ src/oop_75794_Week13_LeonardusAdilla/StudentSerialization.kt
@@ -3,25 +3,39 @@ import java.io.File
 import java.io.FileNotFoundException
 
 data class Student(val name: String, val age: Int, val gpa: Double)
-//Serialization  (Object -> CSV)
+// Serialization (Object -> CSV)
 fun Student.toCsv(): String = "$name, $age, $gpa"
 
 // Deserialization (CSV -> Object)
 fun fromCsv(line: String): Student {
     val parts = line.split(",")
-    return Student(name=parts[0], age=parts[1].toInt(), gpa=parts[2].toDouble())
+    return Student( name = parts[0], age = parts[1].toInt(), gpa = parts[2].toDouble())
+}
 
-    fun saveStudent(student : List<Student>, path: String) {
-        File(path).writeText(student.joinToString("\n") {it.toCsv() })
-    }
+fun saveStudents(students: List<Student>, path: String) {
+    File(path).writeText(students.joinToString("\n") {it.toCsv() })
+}
 
-    fun loadStudent(path: String): List<Student> {
-        return try {
-            File(path).readLines().map {fromCsv(it) }
-        } catch (e: FileNotFoundException) {
-            println("Errror: File tidak  ditemukan! ")
-            emptyList()
-        }
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path).readLines().map { fromCsv(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan")
+        emptyList()
     }
 }
 
+fun main() {
+
+    val students = listOf(
+        Student(name = "Alice", age = 20, gpa = 3.8),
+        Student(name = "Bob", age = 22, gpa = 3.5)
+    )
+    saveStudents(students, path = "Student.csv")
+
+    val loaded = loadStudents(path = "Student.csv")
+
+    println("=== LOADED STUDENT DATA ===")
+
+    loaded.forEach { println(it) }
+}
