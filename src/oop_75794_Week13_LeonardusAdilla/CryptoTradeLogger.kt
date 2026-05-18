50f0f844ebb4b8db308342f1515fea112c0a24c1
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-18 21:59:41 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_75794_Week13_LeonardusAdilla/CryptoTradeLogger.kt
+++ src/oop_75794_Week13_LeonardusAdilla/CryptoTradeLogger.kt
@@ -91,5 +91,9 @@ fun main() {
     File("crypto_trades.csv")
         .appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
 
-    println("Data trade berhasil disimpan.")
+    val loadedData = loadTrades("crypto_trades.csv")
+
+    val totalPnl = loadedData.sumOf { it.pnl }
+
+    println("Total Net PnL: $totalPnl")
 }
