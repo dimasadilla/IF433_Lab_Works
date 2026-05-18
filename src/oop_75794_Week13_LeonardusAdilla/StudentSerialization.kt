2169a113009bd58453a29f5f01bf41db641e81db
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-18 20:15:43 +0700

    week13: create save and  load list functions

--- src/oop_75794_Week13_LeonardusAdilla/StudentSerialization.kt
+++ src/oop_75794_Week13_LeonardusAdilla/StudentSerialization.kt
@@ -10,5 +10,18 @@ fun Student.toCsv(): String = "$name, $age, $gpa"
 fun fromCsv(line: String): Student {
     val parts = line.split(",")
     return Student(name=parts[0], age=parts[1].toInt(), gpa=parts[2].toDouble())
+
+    fun saveStudent(student : List<Student>, path: String) {
+        File(path).writeText(student.joinToString("\n") {it.toCsv() })
+    }
+
+    fun loadStudent(path: String): List<Student> {
+        return try {
+            File(path).readLines().map {fromCsv(it) }
+        } catch (e: FileNotFoundException) {
+            println("Errror: File tidak  ditemukan! ")
+            emptyList()
+        }
+    }
 }
 
