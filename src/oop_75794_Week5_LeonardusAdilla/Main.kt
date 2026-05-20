72ad7ca9719faa38624642e7721dc2cdca30d699
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-20 10:09:05 +0700

    week5: (task1) test overloading in main

--- src/oop_75794_Week5_LeonardusAdilla/Main.kt
+++ src/oop_75794_Week5_LeonardusAdilla/Main.kt
@@ -1,29 +1,46 @@
 package oop_75794_Week5_LeonardusAdilla
 
 fun main() {
-    val dosen1 = Dosen("Pak Alex", "0123456")
-    val admin1 = Admin("Bu Siti")
 
-    // Polmorphic Collection: List yang berisi tipe Parent, tapi isinya objek Anak
+    val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
+    val admin1 = Admin(nama = "Bu Siti")
+
     val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
 
     println("=== AKTIVITAS PEGAWAI ===")
+
     for (pegawai in daftarPegawai) {
-        // Pemanggilan Runtime Polmorphism
+
         pegawai.bekerja()
 
-        // pegawai.mengajar() //  INI AKAN ERROR karena tipe referensinya adalah Pegawai
-        //Smart Casting dengan is dan when
         when (pegawai) {
+
             is Dosen -> {
-                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
-                pegawai.mengajar() // Smart cast! Tidak perlu manual casting (as)
+                println("-> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
             }
+
             is Admin -> {
-                println("=> Terdeteksi sebagai Admin")
+                println("-> Terdeteksi sebagai Admin")
                 pegawai.doAdminWork()
             }
         }
-        println("-------------------------")
+
+        println("----------------------------")
     }
+
+    // ==================================
+    // Method Overloading
+    // ==================================
+
+    val mathHelper = MathHelper()
+
+    val luasPersegi = mathHelper.hitungLuas(4)
+    println("Luas Persegi = $luasPersegi")
+
+    val luasPersegiPanjang = mathHelper.hitungLuas(6, 3)
+    println("Luas Persegi Panjang = $luasPersegiPanjang")
+
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+    println("Luas Lingkaran = $luasLingkaran")
 }
