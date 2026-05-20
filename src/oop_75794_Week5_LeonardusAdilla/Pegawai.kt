8aed2679dc24e82d800e757ad81b635d172dff29
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-20 09:32:16 +0700

    week5: create abstract class Pegawai

---
+++ src/oop_75794_Week5_LeonardusAdilla/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_75794_Week5_LeonardusAdilla
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
