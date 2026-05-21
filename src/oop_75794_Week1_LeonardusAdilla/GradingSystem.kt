ab5a234cf353baf21410c7ae4d0bfa27105475b1
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-21 22:47:18 +0700

    week1:initial setup and procedural code

---
+++ src/oop_75794_Week1_LeonardusAdilla/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_75794_Week1_LeonardusAdilla
+
+fun main() {
+    var name: String = "Jhon Thor"
+    var score: Int= 80
+
+    // Concatenation gaya lama
+    println("Nama" + name + ", Nilai: " + score)
+}
