1d626cec6a993a5931cd5dc9a9be612ae57296e9
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-21 23:24:15 +0700

    week1: (tugas 1) clean up unused argos and comments

--- src/oop_75794_Week1_LeonardusAdilla/CricleCalculator.kt
+++ src/oop_75794_Week1_LeonardusAdilla/CricleCalculator.kt
@@ -1,22 +1,16 @@
 package oop_75794_Week1_LeonardusAdilla
 
-fun main(args: Array<String>) {
+fun main() {
 
-    // Variable definition
     val radius = 7.0
     val pi = 3.14
 
-    // Calculation
     val area = pi * radius * radius
 
-    // String Template
     println("Radius: $radius, Area: $area")
-
-    // Print hasil function
     println(checkSize(area))
 }
 
-// Expression Body Function
 fun checkSize(area: Double) =
     if (area > 100) {
         "This is a Big Circle"
