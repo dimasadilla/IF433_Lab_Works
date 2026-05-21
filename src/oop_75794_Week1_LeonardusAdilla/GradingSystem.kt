7fbc9ee4d1de06f5c7d77076fb5ab27170ce57f6
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-21 22:50:16 +0700

    week1: refactor to use val and string templates

--- src/oop_75794_Week1_LeonardusAdilla/GradingSystem.kt
+++ src/oop_75794_Week1_LeonardusAdilla/GradingSystem.kt
@@ -1,9 +1,10 @@
 package oop_75794_Week1_LeonardusAdilla
 
 fun main() {
+    // REFACTOR: Gunakan dan bagus tipe data eksplisit
     var name: String = "Jhon Thor"
     var score: Int= 80
 
-    // Concatenation gaya lama
-    println("Nama" + name + ", Nilai: " + score)
+    // REFACTOR: String Template($Name)
+    println("Nama $name , Nilai:  $score")
 }
