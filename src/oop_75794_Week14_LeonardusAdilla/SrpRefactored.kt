3d43d25c86688296fb6a52939fb2011f8b6332cc
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-29 12:39:27 +0700

    week14: extract EmailService to fix SRP

--- src/oop_75794_Week14_LeonardusAdilla/SrpRefactored.kt
+++ src/oop_75794_Week14_LeonardusAdilla/SrpRefactored.kt
@@ -1,12 +1,14 @@
 package oop_75794_Week14_LeonardusAdilla
 
-class userValidator {
-    fun validate(user: User): Boolean = user.email.contains("@") && user.age >= 18
-}
-
 class UserRepostory {
     fun save (user: User) {
         println("Saving user: ${user.name} to Database")
+    }
+}
+
+class EmailService {
+    fun sendWelcome(user: User) {
+        println("welcome email -> ${user.email}")
     }
 }
 
