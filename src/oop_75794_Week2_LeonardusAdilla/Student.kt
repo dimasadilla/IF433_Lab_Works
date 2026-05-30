995cb259ce99516143cc2fa9f7ae113cb2b01292
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-30 22:13:35 +0700

    week2: add secondary constructor

--- src/oop_75794_Week2_LeonardusAdilla/Student.kt
+++ src/oop_75794_Week2_LeonardusAdilla/Student.kt
@@ -1,6 +1,7 @@
 package oop_75794_Week2_LeonardusAdilla
 
-class Student (val name: String, val nim: String, var major: String ) {
+class Student(val name: String, val nim: String, var major: String) {
+
     init {
         // Validasi Sederhana: Cek panjang NIM
         if (nim.length != 5) {
@@ -9,5 +10,10 @@ class Student (val name: String, val nim: String, var major: String ) {
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di memory.")
         }
+    }
+
+    // Secondary Constructor
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }
