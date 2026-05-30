c562405e1c85820cb8a0f7f5e9dabb08add23672
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-05-30 22:00:56 +0700

    week2: implement user input and pre-validation

---
+++ src/oop_75794_Week2_LeonardusAdilla/Main.kt
@@ -0,0 +1,33 @@
+package oop_75794_Week2_LeonardusAdilla
+
+import java.util.Scanner
+
+fun main () {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine() // Bersihkan buffer newline (penyakit klasik scanner!)
+    // Validasi di sisi pemanggil (Main)
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter! ")
+        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
+    } else {
+        print("Masukan Jurusan: ")
+        val major = scanner.nextLine()
+
+        // Instansiasi Objek karena data sudah aman
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
+
+
+
+
