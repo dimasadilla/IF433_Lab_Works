f8a2483172e692310010bb0a96f086070e15b434
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-06-02 19:12:34 +0700

    week4: (task 1) test ElectricCar in main

--- src/oop_75794_Week4_LeonardusAdilla/Main.kt
+++ src/oop_75794_Week4_LeonardusAdilla/Main.kt
@@ -1,14 +1,25 @@
 package oop_75794_Week4_LeonardusAdilla
 
 fun main() {
-    println("--- Testing Vehcile ---")
-    val generalVehicle = Vehicle("speed Onthel")
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
     generalVehicle.honk()
     generalVehicle.accelerate()
 
     println("\n--- Testing Car ---")
-    var myCar = Car("Toyota", numberOfDoors = 4)
-    myCar.openTrunk() // Memanggil method milik sendiri
-    myCar.honk()     // Memanggil method yang sudah di-override
-    myCar.accelerate() //  Memanggil gabungan method Parent dan Child
+    val myCar = Car("Toyota", numberOfDoors = 4)
+    myCar.openTrunk()      // Method milik Car
+    myCar.honk()           // Method yang di-override
+    myCar.accelerate()     // Gabungan Parent dan Child
+
+    println("\n--- Testing ElectricCar ---")
+    val myElectricCar = ElectricCar(
+        brand = "Tesla",
+        numberOfDoors = 4,
+        batteryCapacity = 85
+    )
+
+    myElectricCar.accelerate() // Method final yang di-override di ElectricCar
+    myElectricCar.honk()       // Inherit dari Car
+    myElectricCar.openTrunk()  // Inherit dari Car
 }
