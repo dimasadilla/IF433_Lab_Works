af6e43238f6c39103139d12c24e031fc7dd468f8
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-06-02 18:45:14 +0700

    week3: (task 1) implement Weapon testing in main

--- src/oop_75794_Week3_LeonardusAdilla/Main.kt
+++ src/oop_75794_Week3_LeonardusAdilla/Main.kt
@@ -1,16 +1,16 @@
 package oop_75794_Week3_LeonardusAdilla
 
 fun main() {
-    val e = Employee("Budi")
+    val w = Weapon("Dragon Slayer", 500)
 
-    // 1. Test Validasi Salary
-    e.salary = 5000000
-    println("Gaji: ${e.salary}")
+    // 1. Test Damage Negatif
+    w.damage = -50
+    println("Damage setelah set -50: ${w.damage}")
 
-    // 2. Test Encapsulation
-    e.incereasePerformance()
-    //e.performanceRating = 5 // coba uncomment, pasti Merah (Error)
+    // 2. Test Damage Overpowered
+    w.damage = 9999
+    println("Damage setelah set 9999: ${w.damage}")
 
     // 3. Test Computed Property
-    println("Pajak yang harus dibayar: ${e.tax}")
+    println("Tier Senjata: ${w.tier}")
 }
