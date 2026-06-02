21664d06222553a22eb353c3a76cbd179023694d
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-06-02 18:26:03 +0700

    week3: fix setter using backing field

--- src/oop_75794_Week3_LeonardusAdilla/Employee.kt
+++ src/oop_75794_Week3_LeonardusAdilla/Employee.kt
@@ -1,10 +1,13 @@
 package oop_75794_Week3_LeonardusAdilla
 
 class Employee(val name: String) {
-    val salary: Int = 0
+    var salary: Int = 0
         set(value) {
-            println("Mencoba set gaji ke: $value")
-            // PERINGATAN: KODE DI BAWAH INI SALAH & BERBAHAYA!
-            // Kita memanggil setter di dalam setter (Recursive)
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0 // Gunakan field, bukan this. salary
+            } else {
+                field = value
+            }
         }
 }
