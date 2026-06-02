255fb79b438426b3e58b5f6fe5dfb07d1600714c
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-06-02 20:08:09 +0700

    week7: (task) create GameManager singletion

---
+++ src/oop_75794_Week7_LeonardusAdilla/GameManager.kt
@@ -0,0 +1,15 @@
+package oop_75794_Week7_LeonardusAdilla
+
+object GameManager {
+
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
