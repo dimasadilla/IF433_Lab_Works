9408722e0742e7af4159b6b591d750a610fcf0c2
dimasadilla <128572887+dimasadilla@users.noreply.github.com>
2026-06-02 18:18:15 +0700

    week3: simulate stack overflow error in setter

--- src/oop_75794_Week3_LeonardusAdilla/Employee.kt
+++ src/oop_75794_Week3_LeonardusAdilla/Employee.kt
@@ -2,4 +2,9 @@ package oop_75794_Week3_LeonardusAdilla
 
 class Employee(val name: String) {
     val salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE DI BAWAH INI SALAH & BERBAHAYA!
+            // Kita memanggil setter di dalam setter (Recursive)
+        }
 }
---
+++ src/oop_75794_Week3_LeonardusAdilla/Main.kt
@@ -0,0 +1,7 @@
+package oop_75794_Week3_LeonardusAdilla
+
+fun main() {
+    val e = Employee("Budi")
+    println("---Test Error---")
+    e.salary = 5000 // Ini akan meledakan program Anda
+}
